0f8cdf3a64662c6cc384fbb2cfcbbab98a66e17a
kwasnica <ashlynkwasnica@yahoo.com>
2025-02-24 22:48:21 -0600

    created PHP code to insert user into table
    gets data from the html forms and inserts it into database

---
+++ account.php
@@ -0,0 +1,27 @@
+
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '','db_connect');
+
+// get the post records
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// database call procedure to add user to table
+
+$sql = "CALL proc_insert_user('$firstName','$lastName','$email','$password')";
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Account Created";
+}
+    
+
+?>
+
