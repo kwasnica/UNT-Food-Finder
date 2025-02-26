41c6feaffeda050d10772a5241e8d1b1fc15cf89
soriphoono <soriphoono@gmail.com>
2025-02-26 10:28:20 -0600

    Added devshell, formatted project

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = with inputs; [
+        git-hooks-nix.flakeModule
+        treefmt-nix.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: {
+        pre-commit.settings.hooks = {
+          gpt-commit.enable = true;
+        };
+        treefmt.programs = {
+          prettier.enable = true;
+        };
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+            echo 1>&2 "Welcome to the development shell!"
+          '';
+        };
+      };
+    };
+}
