5e4705ffd89cf1dbd4516604efe447ea76eaaa8f
soriphoono <soriphoono@gmail.com>
2025-02-26 10:58:32 -0600

    feat: Refactor file structure and improve styling for account creation page
    - Updated arrow function parameter to underscore in `index.js`
    - Changed file sending to use `res.sendFile` function with `__dirname` in `index.js`
    - Added Bootstrap CSS and JavaScript libraries for styling and interactivity in `index.html`
    - Renamed `<h1>` element to "Create Account" in `index.html`
    - Added script tag for Bootstrap JavaScript bundle in `index.html`
    - Added labels for form input fields in `index.html`
    - Added a submit button for creating an account in `index.html`

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
